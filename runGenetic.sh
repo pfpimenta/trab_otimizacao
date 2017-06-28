25fb222123e67b88dbbc02bebdfb8509485ac72a
pfpimenta <pedrofolettopimenta@gmail.com>
2017-06-28 20:02:29 -0300

    comandos runGenetic.sh

--- runGenetic.sh
+++ runGenetic.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 echo "running all genetic algorithms"
 python gena.py toy2 seedMoreira 200 > gena_toy2_seedMoreira_200.txt
+python gena.py tkp_instances/U2 seedMoreira 200 > gena_U2_seedMoreira_200.txt
+python gena.py tkp_instances/U100 seedMoreira 200 > gena_U100_seedMoreira_200.txt
+python gena.py tkp_instances/I5 seedMoreira 200 > gena_I5_seedMoreira_200.txt
+python gena.py tkp_instances/I25 seedMoreira 200 > gena_I25_seedMoreira_200.txt
+python gena.py tkp_instances/I72 seedMoreira 200 > gena_I72_seedMoreira_200.txt
+python gena.py tkp_instances/I90 seedMoreira 200 > gena_I90_seedMoreira_200.txt
+python gena.py tkp_instances/I100 seedMoreira 200 > gena_I100_seedMoreira_200.txt
+python gena.py tkp_instances/HB5000 seedMoreira 200 > gena_HB5000_seedMoreira_200.txt
+python gena.py tkp_instances/HB10000 seedMoreira 200 > gena_HB10000_seedMoreira_200.txt
+python gena.py tkp_instances/HB15000 seedMoreira 200 > gena_HB15000_seedMoreira_200.txt
